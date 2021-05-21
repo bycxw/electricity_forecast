77369500754a7508c3a8e6d38b2107b0a3ef5ab5
bycxw <beyondchenxi@163.com>
2021-05-21 22:50:23 +0800

    add comments

--- main.py
+++ main.py
@@ -7,10 +7,10 @@
 import os
 import sys
 import argparse
-import pandas as pd
-import numpy as np
+import pandas as pd # excel in python
+import numpy as np # matlab in python
 from tqdm import tqdm
-import pmdarima as pm
+import pmdarima as pm # arima
 # from pyramid.arima import auto_arima
 
 def parse_args():
@@ -26,16 +26,15 @@ def load_dataset(file):
     :param file: str Path to the data file.
     :return:
     """
-    df = pd.read_excel(file, engine="openpyxl", sheet_name=None)
-    data = None
+    df = pd.read_excel(file, engine="openpyxl", sheet_name=None) # 读入excel文件
     sheetnames = ["{}年用电数据".format(year) for year in range(2012, 2021)]
-    industries = df[sheetnames[0]].iloc[:, 0].to_list()
-    data = df[sheetnames[0]].iloc[:, 1:].to_numpy()
+    industries = df[sheetnames[0]].iloc[:, 0].to_list() # 取第一个表格的第一列
+    data = df[sheetnames[0]].iloc[:, 1:].to_numpy() # 取第一个表格的第2列至最后一列
     for sheetname in sheetnames[1:]:
         # for index, row in df[sheetname].iterrows():
         #     row = row.tolist()
         #     data[row[0]] += row[1:]
-        data = np.concatenate((data, df[sheetname].iloc[:, 1:].to_numpy()), axis=-1)
+        data = np.concatenate((data, df[sheetname].iloc[:, 1:].to_numpy()), axis=-1) # 取出每个子表的数据，按时间拼接到一起
     return industries, data
 
 def sum_chunk(x, chunk_size, axis=-1):
@@ -72,32 +71,32 @@ def train_predict(train_data, pred_num, f=None):
     :param train_data:
     :return:
     """
-    arima = pm.auto_arima(train_data, start_p=1, start_q=1, error_action="ignore")
-    print(arima.summary(), file=f)
+    arima = pm.auto_arima(train_data, start_p=1, start_q=1, error_action="ignore")  # 模型训练
+    print(arima.summary(), file=f) # 模型的基本信息
     print('-------------------------------------', file=f)
     print("gold, shape={}".format(train_data.shape), file=f)
     for y in train_data:
         print(y, file=f)
-    preds_in_sample = arima.predict_in_sample()
+    preds_in_sample = arima.predict_in_sample() # 训练数据期间的用电量
     print("\npred in sample, shape={}".format(preds_in_sample.shape), file=f)
     for pred in preds_in_sample:
         print(round(pred, 2), file=f)
-    preds = arima.predict(pred_num)
+    preds = arima.predict(pred_num) # 预测未来用电量
     print("\npred for future(2021-2022), shape={}".format(preds.shape), file=f)
     for pred in preds:
         print(round(pred, 2), file=f)
 
 def main(args):
     print("Load and preprocess data begin...")
-    industries, data = load_dataset(args.dataset_path)
-    data_month = data_transform(industries, data)
+    industries, data = load_dataset(args.dataset_path) # 把数据从excel表格里，以numpy 矩阵的形式放入内存
+    data_month = data_transform(industries, data) # 数据处理成模型需要的形式（按月、季度、年聚合）
     data_season = data_transform(industries, data, granularity="season")
     data_year = data_transform(industries, data, granularity="year")
     print("Load and preprocess data done.")
     print("Train and predict...")
     for industry in tqdm(industries):
         with open(os.path.join(args.save_dir, "{}_month.txt".format(industry)), 'w', encoding='utf-8') as f:
-            train_predict(data_month[industry], 24, f)
+            train_predict(data_month[industry], 24, f) # 训练模型，并预测未来
         with open(os.path.join(args.save_dir, "{}_season.txt".format(industry)), 'w', encoding='utf-8') as f:
             train_predict(data_season[industry], 8, f)
         with open(os.path.join(args.save_dir, "{}_year.txt".format(industry)), 'w', encoding='utf-8') as f:
@@ -112,3 +111,6 @@ if __name__ == "__main__":
         os.makedirs(args.save_dir)
     main(args)
 
+# git github
+# conda
+# pip
